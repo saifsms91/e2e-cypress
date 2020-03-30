b78ed83141b7817c895da29e186644a006d8a51f
saifsms91 <saifsms91@rediffmail.com>
2020-03-31 00:19:56 +0800

    Necessary json file for cypress

---
+++ cypress/integration/first_test.spec.js
@@ -0,0 +1,11 @@
+describe('' , () => {
+    it('' , () => {
+
+        cy.visit('https://www.airbnb.com/')
+        cy.get('._ubzwse > ._14i3z6h')
+        .invoke('text')
+        .should('equal', 'Book unique places to stay and things to do.')
+    });
+
+
+});
