2ae09efd68524d091a15c0f48a8f1078a718eb47
saifsms91 <saifsms91@rediffmail.com>
2020-04-01 14:12:05 +0800

    WIP

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -7,5 +7,14 @@ describe('' , () => {
         .should('equal', 'Book unique places to stay and things to do.')
     });
 
+       it('' , () => {
 
+            cy.get(':nth-child(2) > ._slilk2 > form > ._xp659v > :nth-child(1) > ._bujmdv > [dir="ltr"] > label > ._1u1fi4p > #Koan-magic-carpet-koan-search-bar__input')
+            .type('Kuala Lumpur')
+            cy.get('#Koan-magic-carpet-koan-search-bar__option-1').focus().click()
+   cy.get('._1lds9wb > ._1svux14 > ._p5jgym > tbody > :nth-child(5) > ._16zigr23').click()
+
+cy.get('[aria-label="Choose Wednesday, April 29, 2020 as your end date. "]').focus().click()
+
+ });
 });
