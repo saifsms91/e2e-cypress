2e0f078f91d05028d640c64a722c0a9299b847dc
saifsms91 <saifsms91@rediffmail.com>
2020-04-01 21:58:21 +0800

    Added before-each.spec.js file to run mandatory test before running any test

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -12,9 +12,10 @@ describe('' , () => {
             cy.get(':nth-child(2) > ._slilk2 > form > ._xp659v > :nth-child(1) > ._bujmdv > [dir="ltr"] > label > ._1u1fi4p > #Koan-magic-carpet-koan-search-bar__input')
             .type('Kuala Lumpur')
             cy.get('#Koan-magic-carpet-koan-search-bar__option-1').focus().click()
-   cy.get('._1lds9wb > ._1svux14 > ._p5jgym > tbody > :nth-child(5) > ._16zigr23').click()
+   //cy.xpath('//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div[2]//div[1]//table[1]//tbody[1]//tr[5]//td[5]').last().click({force: true} )
 
-cy.get('[aria-label="Choose Wednesday, April 29, 2020 as your end date. "]').focus().click()
+cy.get('[data-testid=" May 29, 2020"]')
+
 
  });
-});
+})
