fe2273cbf05fa1c825e02ea4d4d0366bf5247cf1
saifsms91 <saifsms91@rediffmail.com>
2020-04-03 17:29:08 +0800

    Code Refactor

--- cypress/integration/before-each.spec.js
+++ cypress/integration/before-each.spec.js
@@ -1,8 +1,11 @@
-describe('' , () => {
-    it('' , () => {
+describe('Open Airbnb page' , () => {
 
-        cy.visit('https://www.airbnb.com/')
-        cy.get('._ubzwse > ._14i3z6h')
+var homepageText = '._ubzwse > ._14i3z6h'
+    it('Launch airbnb and validate homepage heading' , () => {
+
+
+       cy.visit('/')
+        cy.get(homepageText)
         .invoke('text')
         .should('equal', 'Book unique places to stay and things to do.')
 
