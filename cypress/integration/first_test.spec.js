737aecac6eb77138f5962dadc64117e39599f9b9
saifsms91 <saifsms91@rediffmail.com>
2020-04-05 23:04:10 +0800

    Code Cleanup

--- cypress/integration/first_test.spec.js
+++ cypress/integration/first_test.spec.js
@@ -1,21 +1,32 @@
-describe('' , () => {
-    it('' , () => {
+describe('Select listing to stay' , () => {
 
-        cy.visit('https://www.airbnb.com/')
-        cy.get('._ubzwse > ._14i3z6h')
-        .invoke('text')
-        .should('equal', 'Book unique places to stay and things to do.')
-    });
+var clickLocationField = ':nth-child(2) > ._slilk2 > form > ._xp659v > :nth-child(1) > ._bujmdv > [dir="ltr"] > label > ._1u1fi4p > #Koan-magic-carpet-koan-search-bar__input'
+var selectLocationFromSuggestion = '#Koan-magic-carpet-koan-search-bar__option-1'
 
-       it('' , () => {
 
-            cy.get(':nth-child(2) > ._slilk2 > form > ._xp659v > :nth-child(1) > ._bujmdv > [dir="ltr"] > label > ._1u1fi4p > #Koan-magic-carpet-koan-search-bar__input')
+       it('Enter Location' , () => {
+
+            cy.get(clickLocationField)
             .type('Kuala Lumpur')
-            cy.get('#Koan-magic-carpet-koan-search-bar__option-1').focus().click()
-   //cy.xpath('//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div[2]//div[1]//table[1]//tbody[1]//tr[5]//td[5]').last().click({force: true} )
-
-cy.get('[data-testid=" May 29, 2020"]')
-
+            cy.get(selectLocationFromSuggestion).focus().click()
 
  });
+
+
+  it('Select  Date' , () => {
+
+             cy.get('#Koan-magic-carpet-koan-search-bar__option-1').focus().debug()//.click()
+
+    //cy.xpath('//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div//div[2]//div[1]//table[1]//tbody[1]//tr[5]//td[5]').last().click({force: true} )
+
+ cy.get('[data-testid=" May 29, 2020"]')
+
+  });
+
+
+
+
 })
+
+//set env variables
+//activating code completion
